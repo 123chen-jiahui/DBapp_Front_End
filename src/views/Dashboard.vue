2d3aeda909514376f9aa203c2668f027a382f079
chen <1748567982@qq.com>
2022-08-29 00:39:52 +0800

    向oss发送存储请求模板

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,97 @@
 <template>
-  <h1>欢迎使用同济大学医院管理系统</h1>
+<div>
+  <el-upload action='' :on-change="getFile" :limit="1" list-type="picture" :auto-upload=false>
+    <el-button size="small">选择图片上传</el-button>
+  </el-upload>
+  <el-button @click="changeImg"><u>提交修改</u></el-button>
+  </div>
 </template>
 
+
+
 <script>
-  // Utilities
-  
-  export default {
-    name: 'DashboardView',
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      imageUrl: '',
+      hostImg:"",//默认为空
+      new_img:"",//用户更改的新头像
+      change_img_show:false,
+    };
+  },
+  methods: {
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
+    },
+    getFile(file)
+    {
+      const isJPG=file.raw.type==='image/jpeg';
+      const isPNG=file.raw.type==='image/png' ;
+      const isLt5M=file.raw.size/1024/2024<5;
+      if(!isJPG&&!isPNG){
+        this.$message.error('上传图片大小不能超过5MB！');
+      }
+      if((isJPG||isPNG)&&isLt5M){
+        this.getBase64(file.raw).then(res=>{
+        });
+      }
+    },
+    getBase64(file)
+    {
+      let sel=this;//promise作用域问题，在promise内部无法获取到this
+
+      return new Promise(function(resolve,reject){
+        let reader=new FileReader();
+
+        reader.readAsDataURL(file);
+        let imgResult="";
+        reader.onload=function(){
+          imgResult=reader.result;
+          //现在就可以调用api进行图像的更新
+
+          sel.new_img=imgResult;
+          sel.change_img_show=true;
+
+          this.new_img=imgResult;//新的头像路径存储
+        };
+        reader.onerror=function (error){
+          reject(error);
+        };
+        reader.onloadend=function (){
+          resolve(imgResult);
+        }
+      });
+    },
+    changeImg: function() {
+      const outerthis = this
+
+      let data = {
+        photo: this.new_img,
+      }
+      let param = new URLSearchParams(data)
+      axios({
+        method: 'post',
+        url: '/staff/test',
+        data: param,
+      }).then(function(response) {
+        console.log(response.status)
+      }).catch(function(error) {
+        outerthis.showMessage('something wrong' + error.message, 'error')
+      })
+    }
   }
+}
 </script>
